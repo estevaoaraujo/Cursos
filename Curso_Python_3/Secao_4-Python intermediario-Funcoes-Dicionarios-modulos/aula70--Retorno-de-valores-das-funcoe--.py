8e84b507a1c5b0533fab8ba97512a5a96d86577d
estevaoaraujo <estevaorpa@gmail.com>
2025-10-22 22:28:13 -0300

    Sobe

---
+++ Curso_Python_3/Secao_4-Python intermediario-Funcoes-Dicionarios-modulos/aula70--Retorno-de-valores-das-funcoe--.py
@@ -0,0 +1,27 @@
+   
+"""
+Retorno de valores das funções (return)
+"""
+
+
+def soma(x, y):
+    if x > 10:
+        return [10, 20]
+    return x + y
+
+
+# variavel = soma(1, 2)
+# variavel = int('1')
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1)
+print(soma2)
+print(soma(11, 55))
+
+
+def somar(x,y):
+    if x >50 and y>100:
+        return x + y
+
+r = somar(60,150)
+print(r)
