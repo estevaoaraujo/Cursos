f942933d4b3e8433a84a4070dec81d66d1536825
estevaoaraujo <estevaorpa@gmail.com>
2025-10-30 21:49:16 -0300

    Sobe

--- Curso_Python_3/Secao_3-Iniciando-na-programação-com-Python/aula54--Introdução-ao-empacotamento-e-desempacotamento--.py
+++ Curso_Python_3/Secao_3-Iniciando-na-programação-com-Python/aula54--Introdução-ao-empacotamento-e-desempacotamento--.py
@@ -3,3 +3,10 @@ Introdução ao empacotamento e desempacotamento
 """
 _, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
 print(nome)
+
+
+
+um , dois, tres, *resto = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(um, dois, tres)
+print(resto)
